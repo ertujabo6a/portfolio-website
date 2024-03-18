32bc5ba25c85c2d1d296a6804d8f62c3fa11808c
Aleksandr Dmitriev <dmitrievaleksandr2001@gmail.com>
2024-03-18 04:40:50 +0100

    feat: add framer motions to menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,8 +1,10 @@
 "use client";
+
 import Link from "next/link";
 import {useState} from "react";
 import Image from "next/image";
 import NavLink from "@/components/navLink";
+import { motion } from "framer-motion";
 
 const links = [
     { url: "/", title: "Home" },
@@ -12,8 +14,61 @@ const links = [
 ];
 
 const Navbar = () => {
-
     const [open,setOpen] = useState(false)
+
+    const topVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+
+    const centerVariants={
+        closed:{
+            opacity:1,
+        },
+        opened:{
+            opacity:0
+        }
+    }
+
+    const bottomVariants={
+        closed:{
+            rotate:0,
+        },
+        opened:{
+            rotate:-45,
+            backgroundColor:"rgb(255,255,255)"
+        }
+    }
+
+    const listVariants = {
+        closed:{
+            x:"100wv",
+        },
+        opened:{
+            x:0,
+            transition:{
+                when:"beforeChildren",
+                staggerChildren:0.2,
+            }
+        }
+    }
+
+    const listItemVariants = {
+        closed:{
+            x:-10,
+            opacity:0
+        },
+        opened:{
+            x:0,
+            opacity: 1
+        }
+    }
+
     return (
         <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
             {/* LINKS */}
@@ -50,18 +105,41 @@ const Navbar = () => {
                 <button className="w-10 h-8 flex flex-col justify-between z-50 relative"
                         onClick={()=>setOpen((prev) => !prev)}
                 >
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
-                    <div className="w-10 h-1 bg-white rounded"></div>
+                    <motion.div
+                        variants={topVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"
+                    ></motion.div>
+                    <motion.div
+                        variants={centerVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded"
+                    ></motion.div>
+                    <motion.div
+                        variants={bottomVariants}
+                        animate={open ? "opened" : "closed"}
+                        className="w-10 h-1 bg-black rounded origin-left"
+                    ></motion.div>
                 </button>
                 {/* MENU LIST*/}
                 {open && (
-                <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
-                    {links.map(link=>(
-                        <Link href={link.url} key={link.title}>{link.title}</Link>
+                <motion.div
+                    variants={listVariants}
+                    initial="closed"
+                    animate="opened"
+                    className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40"
+                >
+                    {links.map((link) => (
+                        <motion.div
+                            variants={listItemVariants}
+                            className="" key={link.title}>
+                            <Link href={link.url}>
+                                {link.title}
+                            </Link>
+                        </motion.div>
                     ))}
-                </div>
-                    )}
+                </motion.div>
+                )}
             </div>
         </div>
     );
